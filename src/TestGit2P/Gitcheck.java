13cb07996e9fa9d6fbf22ab3389a74e52d8d1026
cuneiform-ronakpatel <rp6038272@gmail.com>
2024-09-12 18:51:49 +0530

    hi

---
+++ src/TestGit2P/Gitcheck.java
@@ -0,0 +1,12 @@
+package TestGit2P;
+
+public class Gitcheck 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("this is for git testing");
+	}
+
+}
